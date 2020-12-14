f9ebfb97b49d6471191c0e8dae8acbc43268cf63
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-14 06:10:01 -0500

    Added files

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const db = require("./models");
+const session = require("express-session");
+const passport = require("passport");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+// import routes
+const routes = require("./routes");
+
+// set up PORT
+const PORT = process.env.PORT || 8080;
+
+// use Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//use handlebars to render
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+
+// set up express-session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// use passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use routes
+app.use(routes)
+
+// connect to database and start server
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`app listening on: http://localhost:${PORT}`);
+
+    })
+})
